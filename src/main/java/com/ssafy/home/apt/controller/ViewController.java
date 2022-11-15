0fbb62be787b70cae73e8abb879073a623bd0e6d
joen00 <joen00@naver.com>
2022-11-15 17:54:32 +0900

    f

--- src/main/java/com/ssafy/home/apt/controller/ViewController.java
+++ src/main/java/com/ssafy/home/apt/controller/ViewController.java
@@ -1,12 +1,15 @@
 package com.ssafy.home.apt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ssafy.home.apt.dto.HousedealinfoDTO;
 import com.ssafy.home.apt.service.AptService;
 
 @Controller
@@ -17,16 +20,9 @@ public class ViewController {
 	AptService aptService;
 	
 	@GetMapping("/houseRead")
-	public ModelAndView houseRead(@RequestParam(value="apartmentName") String apartmentName, @RequestParam String floor, @RequestParam int dealYear, @RequestParam int dealMonth){
-		System.out.println(apartmentName);
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("houseinfo", aptService.gethouseRead(apartmentName, floor, dealYear, dealMonth));
-		System.out.println("여기"+aptService.gethouseRead(apartmentName, floor, dealYear, dealMonth));
-		mv.setViewName("detail");
-		
-		return mv;
+	public ResponseEntity<HousedealinfoDTO> houseRead(@RequestParam(value="apartmentName") String apartmentName, @RequestParam String floor, @RequestParam int dealYear, @RequestParam int dealMonth){		
+		return new ResponseEntity<HousedealinfoDTO>(aptService.gethouseRead(apartmentName, floor, dealYear, dealMonth), HttpStatus.ACCEPTED);
 	}
-	
 	@GetMapping("/join")
 	public String signup() {
 		return "joinForm";
