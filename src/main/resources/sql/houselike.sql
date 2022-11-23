dc2c83b3709225f42cf4aa3f9e8c9e01a6d444e2
lancelot1672 <lancelot1672@naver.com>
2022-11-23 15:49:11 +0900

    sql

--- src/main/resources/sql/houselike.sql
+++ src/main/resources/sql/houselike.sql
@@ -8,3 +8,11 @@ create table houselike(
 
 insert into houselike (user_id, housecode) values ('ssafy',112002102000013);
 
+select *
+from houselike as hl join houseView as hv on hl.housecode = hv.no where user_id = 'ssafy';
+
+create view houseView as select hd.no, hi.apartmentName, hd.floor, hi.dong as 'dongName', hd.dealYear, hd.dealMonth, hd.area, hd.dealamount, hi.lat, hi.lng, hd.aptcode
+                         from housedeal hd join houseinfo hi
+                                                on hd.aptcode = hi.aptcode;
+
+select * from houseView
